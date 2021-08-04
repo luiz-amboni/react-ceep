d9980c0d5e5a5ed4c1a1c2a5d2f4378260e9beb7
Luiz Otavio Amboni <58530274+luiz-amboni@users.noreply.github.com>
2021-08-04 03:06:46 -0300

    Adicionando os estilos e organizando os arquivos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import FormularioCadastro from './componets/FormularioCadastro';
-import ListaDeNotas from './componets/ListaDeNotas';
-
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
-
   render() {
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro />
         <ListaDeNotas />
       </section>
     );
   }
-
 }
 
 export default App;
